a56dd459e89331e0e6919e32ca615dcea138f8cf
jolupa <Jolupa2020u@gmail.com>
2025-07-23 11:39:30 +0200

    base de datos conectada con render correctamente

--- db.js
+++ db.js
@@ -1,45 +1,22 @@
-// db.js
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'test_user',
-  host: 'localhost',
-  database: 'test',
-  password: '12345',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL, // usamos variable de entorno
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-async function getUsers(){
-      const result = await pool.query('SELECT * FROM users');
-      return result;
-}
-
-async function getMessages() {
-  const result = await pool.query('SELECT * FROM messages')
-  return result.rows
-}
-
-async function postMessage(mensaje) {
-    if (!mensaje || typeof mensaje !== 'string') {
-    throw new Error('Mensaje inválido');
-  }
-
-  const result = await pool.query(
-  'INSERT INTO messages (date, text) VALUES (NOW(), $1)',
-  [mensaje]
-  );
-
-  console.log("mensaje añadido")
-}
-
-function truncateMessages() {
-  pool.query('TRUNCATE messages')
-}
-
 module.exports = {
-  pool,
-  getUsers,
-  getMessages,
-  postMessage,
-  truncateMessages
+  query: (text, params) => pool.query(text, params),
+  getMessages: async () => {
+    const res = await pool.query('SELECT * FROM messages');
+    return res.rows;
+  },
+  postMessage: async (message) => {
+    await pool.query('INSERT INTO messages (text) VALUES ($1)', [message]);
+  },
+  truncateMessages: async () => {
+    await pool.query('TRUNCATE messages');
+  }
 };
